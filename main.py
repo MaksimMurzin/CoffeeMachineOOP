49fa89d3fdd4bf061629d621a85c04ef82f325b2
MaksimMurzin <71095366+MaksimMurzin@users.noreply.github.com>
2020-12-03 00:53:00 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,24 @@ from money_machine import MoneyMachine
 
 isOn = True
 menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
 
 
 while isOn :
+    choice = input(f"\nWhich drink would you chose? {menu.get_items()}: ")
+    if( choice == "report"):
+        print(coffee_maker.report())
+        print(money_machine.report())
+        continue
+    elif choice == "off":
+        print("Powering off")
+        isOn = False
+        continue
+    choice = menu.find_drink(choice)
+    if choice :
+        if coffee_maker.is_resource_sufficient(choice):
+            coffee_maker.make_coffee(choice)
+
+
